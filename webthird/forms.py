ff684af5d418671f3298d3931d441645d3489205
ye0nw0 <yeonk982@gmail.com>
2021-12-19 10:28:35 +0900

    project commit

--- webthird/forms.py
+++ webthird/forms.py
@@ -35,17 +35,35 @@ class ReviewForm(ModelForm):
 class RestaurantForm(ModelForm):
     class Meta:
         model = Restaurant
-        fields = ['name', 'address']
+        fields = ['name', 'address', 'image', 'password']
         labels = {
             'name' : _('이름'),
             'address' : _('주소'),
+            'image' : _('이미지 url'),
+            'password' : _('게시물 비밀번호'),
         }
         help_texts = {
             'name' : _('이름을 입력하세요.'),
             'address' : _('주소를 입력하세요.'),
+            'image' : _('이미지의 url을 입력해주세요.'),
+            'password' : _('게시물 비밀번호를 입력해주세요.'),
+        }
+        widgets = {
+            'password' : forms.PasswordInput()
         }
         error_messages = {
             'name' : {
                 'max_length' : _('이름이 너무 깁니다.')
-            }
+            },
+            'image' : {
+                'max_length' : _('이미지 주소가 너무 깁니다. 500자 이하로 입력해주세요.')
+            },
+            'password' : {
+                'max_length' : _('비밀번호가 너무 깁니다. 20자 이하로 설정해주세요.')
+            },
         }
+
+class RestaurantUpdateForm(RestaurantForm):
+    class Meta:
+        model = Restaurant
+        exclude = ['password']
--- webthird/views.py
+++ webthird/views.py
@@ -1,13 +1,15 @@
+from django.core import paginator
 from django.forms.widgets import NullBooleanSelect
 from django.shortcuts import redirect, render, get_object_or_404
 from webthird.models import Restaurant, Review
 from django.core.paginator import Paginator
-from webthird.forms import RestaurantForm, ReviewForm
+from webthird.forms import RestaurantForm, ReviewForm, RestaurantUpdateForm
 from django.http import HttpResponseRedirect
-# Create your views here.
+from django.db.models import Count, Avg
+
 
 def list(request):
-    restaurants = Restaurant.objects.all()
+    restaurants = Restaurant.objects.all().annotate(reviews_count=Count('review')).annotate(average_point=Avg('review__point'))
     paginator = Paginator(restaurants, 5)
 
     page = request.GET.get('page')
@@ -32,8 +34,9 @@ def update(request):
     if request.method == 'POST' and 'id' in request.POST:
         #item = Restaurant.objects.get(pk = request.POST.get('id'))
         item = get_object_or_404(Restaurant, pk = request.POST.get('id'))
-        form = RestaurantForm(request.POST, instance=item)
-        if form.is_valid():
+        password = request.POST.get('password', '')
+        form = RestaurantUpdateForm(request.POST, instance=item)
+        if form.is_valid() and password == item.password:
             item = form.save()
     elif request.method == 'GET':
         item = get_object_or_404(Restaurant, pk = request.GET.get('id'))
@@ -50,11 +53,14 @@ def detail(request, id):
     return HttpResponseRedirect('/webthird/list/')
 
 
-def delete(request):
-    if 'id' in request.GET:
-        item = get_object_or_404(Restaurant, pk = request.GET.get('id'))
-        item.delete()
-    return HttpResponseRedirect('/webthird/list/')
+def delete(request,id):
+    item = get_object_or_404(Restaurant, pk = id)
+    if request.method == 'POST' and 'password' in request.POST:
+        if item.password == request.POST.get('password') or item.password is None:
+            item.delete()
+            return redirect('list')
+        return redirect('restaurant-detail', id=id)
+    return render(request, 'webthird/delete.html', { 'item' : item })
 
 
 def review_create(request, restaurant_id):
@@ -67,3 +73,23 @@ def review_create(request, restaurant_id):
     item = get_object_or_404(Restaurant, pk=restaurant_id)
     form = ReviewForm(initial={'restaurant' : item})
     return render(request, 'webthird/review_create.html', {'form' : form, 'item' : item})
+
+
+def review_delete(reqeust, restaurant_id, review_id):
+    item = get_object_or_404(Review, pk=review_id)
+    item.delete()
+
+    return redirect('restaurant-detail', id=restaurant_id)
+
+
+def review_list(request):
+    reviews = Review.objects.all().select_related().order_by('-created_at')
+    paginator = Paginator(reviews, 10)
+
+    page = request.GET.get('page')
+    items = paginator.get_page(page)
+
+    context = {
+        'reviews' : items
+    }
+    return render(request, 'webthird/review_list.html', context)
